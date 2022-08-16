2578aeee73c9ece03155433d221e6108b097504f
Biplobprojects <almightyzeus008@gmail.com>
2022-08-16 22:34:44 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+public class Main{
+
+    int a;
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.a = 12;
+        Main obj2 = new Main();
+        obj2.a =123;
+
+        System.out.println(obj.a+" "+ obj2.a);
+
+
+        
+    }
+}
---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test{
+    static int a;
+    public static void main(String[] args) {
+        //Test obj = new Test();
+        a= 56;
+        //Test obj2 = new Test();
+        a = 54;
+
+        System.out.println(a +" "+ a);
+    }
+}
